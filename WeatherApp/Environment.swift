4e378874e4db7200e9e8cb67e2936d8b43f74afc
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2023-07-08 01:06:55 +0300

    test something.

---
+++ WeatherApp/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Environment.swift
+//  WeatherApp
+//
+//  Created by HardiB.Salih on 7/8/23.
+//
+
+import Foundation
+
+struct Environment {
+    static let apiKey = "a1a07f4a12a519ae093e4ff5b5ca88ae"
+}
+
+//https://api.openweathermap.org/data/2.5/weather?lat=33.44&lon=-94.04&appid=a1a07f4a12a519ae093e4ff5b5ca88ae&units=metric
