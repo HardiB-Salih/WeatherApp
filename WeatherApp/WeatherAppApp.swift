b94b31e44c07da280b069114686ae06bcd91e7f8
HardiB.Salih <30411248+HardiB-Salih@users.noreply.github.com>
2023-07-07 19:25:26 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by HardiB.Salih on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
